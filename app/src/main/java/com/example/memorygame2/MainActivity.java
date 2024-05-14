c2f41723279fad77a4d2eaa736001c96419a25ea
danisigal <dani.sigal1206@gmail.com>
2024-05-14 23:52:37 +0300

    login and game

--- app/src/main/java/com/example/memorygame2/MainActivity.java
+++ app/src/main/java/com/example/memorygame2/MainActivity.java
@@ -1,17 +1,18 @@
 package com.example.memorygame2;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -35,39 +36,43 @@ public class MainActivity extends AppCompatActivity {
     FirebaseAuth auth;
     GoogleSignInClient googleSignInClient;
     ShapeableImageView imageView;
-    TextView name,mail;
-        private final ActivityResultLauncher<Intent>  activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
-            @Override
-            public void onActivityResult(ActivityResult result) {
-                if (result.getResultCode() == RESULT_OK){
-                    Task<GoogleSignInAccount> accountTask = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
-                    try {
-                        GoogleSignInAccount signInAccount = accountTask.getResult(ApiException.class);
-                        AuthCredential authCredential = GoogleAuthProvider.getCredential(signInAccount.getIdToken(), null );
-                        auth.signInWithCredential(authCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(task.isSuccessful()){
-                                    auth = FirebaseAuth.getInstance();
-                                    Glide.with(MainActivity.this).load(Objects.requireNonNull(auth.getCurrentUser()).getPhotoUrl()).into(imageView);
-                                    name.setText(auth.getCurrentUser().getDisplayName());
-                                    mail.setText(auth.getCurrentUser().getEmail());
-                                    Toast.makeText(MainActivity.this,"Signed in successfully!",Toast.LENGTH_SHORT).show();
-                                } else{
-                                    Toast.makeText(MainActivity.this,"Failed to sign in:"+ task.getException() ,Toast.LENGTH_SHORT).show();
-                                }
+    TextView name, mail;
+    private final ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+
+        public void onActivityResult(ActivityResult result) {
+            if (result.getResultCode() == RESULT_OK) {
+                Task<GoogleSignInAccount> accountTask = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
+                try {
+                    GoogleSignInAccount signInAccount = accountTask.getResult(ApiException.class);
+                    AuthCredential authCredential = GoogleAuthProvider.getCredential(signInAccount.getIdToken(), null);
+                    auth.signInWithCredential(authCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                auth = FirebaseAuth.getInstance();
+                                Glide.with(MainActivity.this).load(Objects.requireNonNull(auth.getCurrentUser()).getPhotoUrl()).into(imageView);
+                                name.setText(auth.getCurrentUser().getDisplayName());
+                                mail.setText(auth.getCurrentUser().getEmail());
+                                Toast.makeText(MainActivity.this, "Sign in successfully!", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                                intent.putExtra("USERNAME",auth.getCurrentUser().getDisplayName());
+
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(MainActivity.this, "Failed to sign in: " + task.getException(), Toast.LENGTH_SHORT).show();
                             }
-                        });
-                    } catch (ApiException e) {
-                        e.printStackTrace();
-                    }
+                        }
+                    });
+                } catch (ApiException e) {
+                    e.printStackTrace();
                 }
             }
-        });
-
+        }
+    });
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
         FirebaseApp.initializeApp(this);
@@ -75,20 +80,15 @@ public class MainActivity extends AppCompatActivity {
         name = findViewById(R.id.nameTV);
         mail = findViewById(R.id.mailTV);
 
-        GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.client_id))
-                .requestEmail()
-                .build();
-        googleSignInClient = GoogleSignIn.getClient(MainActivity.this, options);
-
-        auth = FirebaseAuth.getInstance();
-
-        SignInButton signInButton = findViewById(R.id.signIn);
+        GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.client_id)).requestEmail().build() ;
+        googleSignInClient = GoogleSignIn.getClient(MainActivity.this,options);
+        auth = FirebaseAuth.getInstance() ;
+        SignInButton signInButton = findViewById(R.id.signIn) ;
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = googleSignInClient.getSignInIntent();
-                activityResultLauncher.launch(intent);
+                Intent intent = googleSignInClient.getSignInIntent() ;
+                activityResultLauncher.launch(intent) ;
             }
         });
     }
--- app/src/main/java/com/example/memorygame2/MainGame.java
+++ app/src/main/java/com/example/memorygame2/MainGame.java
@@ -1,5 +1,203 @@
-package com.example.memorygame2;
+ package com.example.memorygame2;
 
-public class MainGame {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainGame extends AppCompatActivity {
+
+    String[][] board;
+    ImageView[] ImageViews;
+    int count;
+    int countPlayer1 = 0 ;
+    int countPlayer2 = 0;
+    int card1;
+    String turn = "counterPlayer1";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    private void onNewGame() {
+        board = new String[4][4];
+        for (int row = 0; row < 4; row++)
+            for (int col = 0; col < 4; col++) {
+                board[row][col] = new String();
+            }
+        count=0;
+    }
+
+    private void onNewGame1() {
+        ImageView[] imageViews = new ImageView[8];
+
+        // Array of drawable resource IDs
+        int[] drawableIds = {
+                R.drawable.apple,
+                R.drawable.watermelon,
+                R.drawable.grape,
+                R.drawable.kiwi,
+                R.drawable.mango,
+                R.drawable.peach,
+                R.drawable.pineapple,
+                R.drawable.strawberry
+        };
+
+        for (int i = 0; i < 8; i++) {
+            // Create a new ImageView
+            Context context = null;
+            ImageView imageView = new ImageView(context);
+            // Set the image resource for the ImageView
+            imageView.setImageResource(drawableIds[i]);
+            // Add the ImageView to the array
+            imageViews[i] = imageView;
+        }
+    }
+
+    public void knock(View view) {
+        if (view.getId() == R.id.kiwi1) {
+            count++;
+            ImageView backImageView = findViewById(R.id.kiwi1);
+            backImageView.setImageResource(R.drawable.kiwi);
+            turnEnd(view);
+        }
+        if (view.getId() == R.id.strawberry1) {
+            count++;
+            ImageView backImageView = findViewById(R.id.strawberry1);
+            backImageView.setImageResource(R.drawable.strawberry);
+            turnEnd(view);
+        }
+        if (view.getId() == R.id.mango1){
+            count++;
+            ImageView backImageView = findViewById(R.id.mango1);
+            backImageView.setImageResource(R.drawable.mango);
+            turnEnd(view);
+        }
+        if (view.getId() == R.id.grape1){
+            count++;
+            ImageView backImageView = findViewById(R.id.grape1);
+            backImageView.setImageResource(R.drawable.grape);
+            turnEnd(view);
+        }
+        if (view.getId() == R.id.pineapple1){
+            count++;
+            ImageView backImageView = findViewById(R.id.pineapple1);
+            backImageView.setImageResource(R.drawable.pineapple);
+            turnEnd(view);
+        }
+        if (view.getId() == R.id.watermelon1){
+            count++;
+            ImageView backImageView = findViewById(R.id.watermelon1);
+            backImageView.setImageResource(R.drawable.watermelon);
+            turnEnd(view);
+        }
+        if (view.getId() == R.id.peach1){
+            count++;
+            ImageView backImageView = findViewById(R.id.peach1);
+            backImageView.setImageResource(R.drawable.peach);
+            turnEnd(view);
+        }
+        if (view.getId() == R.id.apple1){
+            count++;
+            ImageView backImageView = findViewById(R.id.apple1);
+            backImageView.setImageResource(R.drawable.apple);
+            turnEnd(view);
+        }
+        if (view.getId() == R.id.kiwi2){
+            count++;
+            ImageView backImageView = findViewById(R.id.kiwi2);
+            backImageView.setImageResource(R.drawable.kiwi);
+            turnEnd(view);
+        }
+        if (view.getId() == R.id.strawberry2){
+            count++;
+            ImageView backImageView = findViewById(R.id.strawberry2);
+            backImageView.setImageResource(R.drawable.strawberry);
+            turnEnd(view);
+        }
+        if (view.getId() == R.id.mango2) {
+            count++;
+            ImageView backImageView = findViewById(R.id.mango2);
+            backImageView.setImageResource(R.drawable.mango);
+            turnEnd(view);
+        }
+        if (view.getId() == R.id.grape2) {
+            count++;
+            ImageView backImageView = findViewById(R.id.grape2);
+            backImageView.setImageResource(R.drawable.grape);
+            turnEnd(view);
+        }
+        if (view.getId() == R.id.pineapple2) {
+            count++;
+            ImageView backImageView = findViewById(R.id.pineapple2);
+            backImageView.setImageResource(R.drawable.pineapple);
+            turnEnd(view);
+        }
+        if (view.getId() == R.id.watermelon2) {
+            count++;
+            ImageView backImageView = findViewById(R.id.watermelon2);
+            backImageView.setImageResource(R.drawable.watermelon);
+            turnEnd(view);
+        }
+        if (view.getId() == R.id.peach2) {
+            count++;
+            ImageView backImageView = findViewById(R.id.peach2);
+            backImageView.setImageResource(R.drawable.peach);
+            turnEnd(view);
+        }
+        if (view.getId() == R.id.apple2) {
+            count++;
+            ImageView backImageView = findViewById(R.id.apple2);
+            backImageView.setImageResource(R.drawable.apple);
+            turnEnd(view);
+        }
+    }
+    public void closeCard(int card1, ImageView view){
+        ImageView card1Closed = findViewById(card1);
+        card1Closed.setImageResource(R.drawable.fruit);
+        ImageView card2Closed = findViewById(view.getId());
+        card2Closed.setImageResource(R.drawable.fruit);
+    }
+    public void turnEnd(View view) {
+        if (count % 2 != 0){
+            card1 = view.getId();
+            return;
+        }
+        if(count % 2 == 0){
+            ImageView imageViewCard1 = findViewById(card1);
+            Drawable image1 = imageViewCard1.getDrawable();
+            Drawable image2 = ((ImageView)view).getDrawable();
+            if(image1.getConstantState().equals(image2.getConstantState())){
+                if(turn.equals("counterPlayer1")){
+                    countPlayer1++;
+                    turn  = "counterPlayer2";
+                }
+                else{
+                    countPlayer2++;
+                    turn = "counterPlayer1";
+                }
+            }
+            else{
+                final Handler handler = new Handler();
+                handler.postDelayed(() -> closeCard(card1,((ImageView) view)), 400);
+                if(turn.equals("counterPlayer1")){
+                    turn  = "counterPlayer2";
+                }
+                else{
+                    turn = "counterPlayer1";
+                }
+            }
+        }
+    }
+
 
 }
